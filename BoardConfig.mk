820dae272fad8f1cf0115516cb6dd1510ebac528
redducc <sushmit718@gmail.com>
2024-03-02 12:52:35 +0000

    coffee: Initial makefile

---
+++ BoardConfig.mk
@@ -0,0 +1,14 @@
+#
+# Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Use the non-open-source parts, if they're present
+-include vendor/realme/coffee/BoardConfigVendor.mk
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/realme/coffee
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Kernel
+KERNEL_PREBUILT_DIR := $(LOCAL_PATH)-kernel
